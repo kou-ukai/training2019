762d811ebf4b1db73f5691853d0b267aef4ed2cf
kobayashi-makoto <47737251+kobayashi-makoto@users.noreply.github.com>
2019-02-19 00:00:40 +0900

    考え中
    各クラスの概要をコメントにまとめ

--- JavaTask/src/jp/co/cisys/training/playingcard/kobayashi/suped.java
+++ JavaTask/src/jp/co/cisys/training/playingcard/kobayashi/suped.java
@@ -2,21 +2,32 @@ package jp.co.cisys.training.playingcard.kobayashi;
 
 import java.util.ArrayList;
 
-
 public class suped {
 	
 	ArrayList<ArrayList<String>> supeds=new ArrayList<ArrayList<String>>();
 	
 	public suped() {
-		
 	
+	}
+
+	public void addSupead(){
+	
+		
+		
 		
 		
 	}
 	
+	//オブジェクと思考製作案まとめ
+	/**
+	/*親抽象orインターフェイスクラスＣＡＲＤ作成
+	/*親orサブにて数字を作成
+	/*サブはカードの種類（親クラスの数字を貰うメソッドを用意1-13　String型・ＭＡＩＮにて使用数値を参照できるメソッド用意・出力メソッドも用意）
+	/*ＭＡＩＮのスイッチ文にてサブの種類選択（大本は引く枚数分だけループ・選択はランダムメソッド使用1-4）
+	/*
+	/**
 	
-	
-	
-	
-
+		
+		
+		
 }
