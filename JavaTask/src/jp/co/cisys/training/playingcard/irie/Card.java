fbbfc2f34a9ed6c0c93bc9a807e92165f242bf52
yuuma.irie <yuuma.irie@cisys.co.jp>
2019-02-19 13:08:05 +0900

    演習

--- JavaTask/src/jp/co/cisys/training/playingcard/irie/Card.java
+++ JavaTask/src/jp/co/cisys/training/playingcard/irie/Card.java
@@ -12,10 +12,8 @@ public class Card {
 				{ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13" } };
 		List<String[]> list = Arrays.asList(card);
 		Collections.shuffle(list);
-		String card2[] = (String[]) list.toArray(new String[52]);
-
-		for (int i = 0; i < card2.length; i++) {
-			System.out.println(card2[i]);
+		for (int i = 0; i < 5; i++) {
+			System.out.print(list.get(i));
 		}
 
 	}
