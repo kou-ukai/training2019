2d8eb5649785972703a154ce8b01186571a67e35
kobayshi.makoto <makoto.kobayshi@cisys.co.jp>
2019-02-25 19:31:08 +0900

    2/25課題終了

--- JavaTask/src/jp/co/cisys/training/playingcard/kobayashi/card/Card.java
+++ JavaTask/src/jp/co/cisys/training/playingcard/kobayashi/card/Card.java
@@ -7,64 +7,64 @@ public class Card {
 
 	//山用のリスト作成
 	private ArrayList<String> yama = new ArrayList<>();
-	
+
 	//カードの種類名作成
-	final  String[] Type= {"クローバー","ダイヤ","スペード","ハート"};
-		
+	final String[] Type = { "クローバー", "ダイヤ", "スペード", "ハート" };
+
 	//カードの数字の種類
-	final String[] Numbers = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-    
+	final String[] Numbers = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+
 	/**
 	 * コンストラクタ
 	 */
 	public Card() {
 
 	}
-	
+
 	/**
 	 * 山を作成+シャッフル
 	 */
 	public void yama() {
-		
-		for(String a:Type) {
-			
-			for(String b:Numbers) {
-				
-			yama.add(a+b);
-			
+
+		//山リストの作成
+		for (String a : Type) {
+
+			for (String b : Numbers) {
+
+				yama.add(a + b);
+
 			}
-			
+
 		}
 
+		//中身をシャッフル
 		Collections.shuffle(yama);
-		
+
 	}
-	
+
+	/**山の枚数を取得
+	 * @return　山の枚数
+	 */
 	public int sizeYama() {
-		
-		int size=0;
-		
-		size=yama.size();
-		
+
+		int size = 0;
+
+		size = yama.size();
+
 		return size;
 	}
-	
-	
-	
 
+	/**山からカードを引く
+	 * @param hand　取得する番号
+	 * @return　カードの名前を取得
+	 */
 	public String getYama(int hand) {
-		
+
 		String hands;
-		
-		hands=yama.get(hand);
-		
+
+		hands = yama.get(hand);
+
 		return hands;
 	}
-	
-	public void removeYama(int hand) {
-		
-		yama.remove(hand);
-		
-	}
-	
+
 }
--- JavaTask/src/jp/co/cisys/training/playingcard/kobayashi/card/Human.java
+++ JavaTask/src/jp/co/cisys/training/playingcard/kobayashi/card/Human.java
@@ -1,43 +1,35 @@
 package jp.co.cisys.training.playingcard.kobayashi.card;
 
-import java.util.Random;
-
 public class Human {
-	
+
 	//カードを取得する枚数
-	final static int Get=5;
-	
-	public static void main(String[]args) {
-		
+	final static int Get = 5;
+
+	public static void main(String[] args) {
+
 		//カードクラスのインスタンス生成
-		Card card=new Card();
-		
+		Card card = new Card();
+
 		//作成及びシャッフル
 		card.yama();
-		
+
 		//手札作成用の変数
-		String[] hand= new String[Get];
-		
-		//手札取得用ランダム変数用意
-		Random rand=new Random(); 
-		
+		String[] hand = new String[Get];
+
 		//山札からカードを取得（指定された枚数まで）
-		for(int i=0;i<Get;i++) {
-			
-			
-			int randomNumeber= rand.nextInt(card.sizeYama());
-			
-			hand[i]=card.getYama(randomNumeber);
-			
-			card.removeYama(randomNumeber);
+		for (int i = 0; i < Get; i++) {
+
+			hand[i] = card.getYama(i);
+
 		}
-		
-		for(String c:hand) {
-			
+
+		//カードを公開
+		for (String c : hand) {
+
 			System.out.println(c);
-			
+
 		}
-		
+
 	}
 
 }
