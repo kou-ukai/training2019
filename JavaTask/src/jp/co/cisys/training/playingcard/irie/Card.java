1484ada62f3727f8c8988cb6eaccc249c11fecff
yuuma.irie <yuuma.irie@cisys.co.jp>
2019-02-26 18:44:25 +0900

    勉強会2/26

---
+++ JavaTask/src/jp/co/cisys/training/playingcard/irie/Card.java
@@ -0,0 +1,48 @@
+package jp.co.cisys.training.playingcard.irie;
+
+public class Card {
+	private int number;
+	private String mark;
+
+	public Card(int number, String mark) {
+		this.number = number;
+		this.mark = mark;
+	}
+
+	public void printCard() {
+		String name;
+		if (number == 1) {
+			name = "A";
+			System.out.println(mark + "の" + name);
+		} else if (number == 11) {
+			name = "J";
+			System.out.println(mark + "の" + name);
+		} else if (number == 12) {
+			name = "Q";
+			System.out.println(mark + "の" + name);
+		} else if (number == 13) {
+			name = "K";
+			System.out.println(mark + "の" + name);
+		} else {
+			System.out.println(mark + "の" + number);
+		}
+
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public String getMark() {
+		return mark;
+	}
+
+	public void setMark(String mark) {
+		this.mark = mark;
+	}
+
+}
