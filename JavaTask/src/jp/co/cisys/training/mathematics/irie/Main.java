4fbb911f50a63d3565257e5c91c46ec7a95eb849
yuuma.irie <yuuma.irie@cisys.co.jp>
2019-02-28 19:46:09 +0900

    勉強会2/28

--- JavaTask/src/jp/co/cisys/training/mathematics/irie/Main.java
+++ JavaTask/src/jp/co/cisys/training/mathematics/irie/Main.java
@@ -6,20 +6,29 @@ public class Main {
 		// TODO 自動生成されたメソッド・スタブ
 
 		// 任意の2つの数字をセットする
-		int a = 15;
-		int b = 12;
-
-		// a <= bにする
-		if (a > b) {
-			int tmp = a;
-			a = b;
-			b = tmp;
+		int[] a = { 15, 12, 70, 156 };
+		int gdc5 = a[0];
+		int lcm = a[0];
+		for (int i = 0; i < a.length ; i++) {
+			// a <= bにする
+			if (gdc5 > a[i]) {
+				int tmp = a[i];
+				a[i] =gdc5;
+				gdc5 = tmp;
+			}
+			Gdc gdc1 = new Gdc(gdc5, a[i]);
+			gdc5= gdc1.getGcd();
+			if (lcm > a[i]) {
+				int tmp = a[i];
+				a[i] =lcm;
+				lcm = tmp;
+			}
+			Gdc gdc2 = new Gdc(lcm, a[i]);
+			lcm = gdc2.getLcm();
 		}
 
-		Gdc gdc = new Gdc(a, b);
-
-		System.out.println(gdc.getGcd());
-		System.out.println(gdc.getLcm());
+		System.out.println(gdc5);
+		System.out.println(lcm);
 	}
 
 }
