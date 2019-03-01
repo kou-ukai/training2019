32f1d5f787a96ab4592dee6b674c7b78c12f38d4
kobayshi.makoto <makoto.kobayshi@cisys.co.jp>
2019-03-01 18:42:02 +0900

    課題修正・・・ループ内の分岐を無くしました

--- JavaTask/src/jp/co/cisys/training/mathematics/kobayashi2/Main.java
+++ JavaTask/src/jp/co/cisys/training/mathematics/kobayashi2/Main.java
@@ -6,43 +6,31 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int[] nums = { 9, 12, 48, 3};
-		
-		int maxGdc=0;// 最大公約数
-		int minGdc=0;//最少公倍数
-		
+		int[] nums = { 9, 12, 3, 48 };
+
+		int gcd = nums[0];// 最大公約数
+		int lcm = nums[0];//最少公倍数
+
+		//配列の要素分ループ
 		for (int i = 1; i < nums.length; i++) {
 
-			switch(i) {
-				case 1:
-					int maxNum=Math.max(nums[0],nums[i]);
-					int minNum=Math.min(nums[0],nums[i]);
+			//数値の入れ替え
+			int maxNum = Math.max(gcd, nums[i]);
+			int minNum = Math.min(gcd, nums[i]);
 
-					Gdc gdc = new Gdc(maxNum, minNum);	
+			//max最大公約数　min最小公倍数
+			Gdc max = new Gdc(maxNum, minNum);
+			Gdc min = new Gdc(lcm, nums[i]);
 
-					maxGdc = gdc.getGcd();
-					minGdc=gdc.getLcm();
-					break;
-
-		        //2回目以降１回目の数値を元に計算を行う為
-                default:
-                	
-                	maxNum=Math.max(maxGdc, nums[i]);
-    				minNum=Math.min(maxGdc,nums[i]);
-
-                	Gdc max = new Gdc(maxNum, minNum);
-					Gdc min = new Gdc(minGdc,nums[i]);
-					
-					maxGdc=max.getGcd();
-					minGdc=min.getLcm();
-					break;
-
-			}
+			//最大公約数と最小公倍数の取得
+			gcd = max.getGcd();
+			lcm = min.getLcm();
 
 		}
 
-		System.out.println("最大公約数" + maxGdc);
-		System.out.println("最小公倍数" + minGdc);
+		//結果の出力
+		System.out.println("最大公約数" + gcd);
+		System.out.println("最小公倍数" + lcm);
 
 	}
 
