d04e614f71219bac0958c511856308686e654b53
yuuma.irie <yuuma.irie@cisys.co.jp>
2019-03-01 19:30:51 +0900

    3/1

--- JavaTask/src/jp/co/cisys/training/mathematics/irie/Main.java
+++ JavaTask/src/jp/co/cisys/training/mathematics/irie/Main.java
@@ -6,11 +6,11 @@ public class Main {
 		// TODO 自動生成されたメソッド・スタブ
 
 		// 任意の数の数字をセットする
-		int[] numbers = { 15, 12, 70, 156 };
+		int[] numbers = { 15, 12, 70, 157 };
 		int max = numbers[0]; //最大公約数
 		int min = numbers[0]; //最小公倍数
-
 		for (int i = 0; i < numbers.length; i++) {
+			
 			// max <= numbers[i]にする
 			if (max > numbers[i]) {
 				int tmp = numbers[i];
@@ -21,13 +21,14 @@ public class Main {
 			max = gdc1.getGcd(); //最大公約数を求めてmaxに代入
 
 			// min <= numbers[i]にする
+			int num=numbers[i];
 			if (min > numbers[i]) {
 				int tmp = numbers[i];
-				numbers[i] = min;
+				num = min;
 				min = tmp;
 			}
-			Gdc gdc2 = new Gdc(min, numbers[i]);
-			min = gdc2.getLcm(); //最小公倍数を求めてminに代入
+			Gdc gdc2 = new Gdc(min, num);
+			min = gdc2.getLcm();//最小公倍数を求めてminに代入
 		}
 
 		System.out.println(max);
