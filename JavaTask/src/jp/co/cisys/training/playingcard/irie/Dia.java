7d34a9276d75c55e6c2e3dc06103ef19f0859a1c
yuuma.irie <yuuma.irie@cisys.co.jp>
2019-03-04 19:17:11 +0900

    勉強会3/4

---
+++ JavaTask/src/jp/co/cisys/training/playingcard/irie/Dia.java
@@ -0,0 +1,8 @@
+package jp.co.cisys.training.playingcard.irie;
+
+public class Dia extends Card {
+	public Dia(int number) {
+		super(number, "ダイヤ");
+	}
+
+}
---
+++ JavaTask/src/jp/co/cisys/training/playingcard/irie/Heart.java
@@ -0,0 +1,9 @@
+package jp.co.cisys.training.playingcard.irie;
+
+public class Heart extends Card {
+	public Heart(int number) {
+		super(number, "ハート");
+	}
+
+
+}
