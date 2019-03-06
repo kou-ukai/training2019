9bc1a796191321b8569257d04dd29f714d518277
hir-ikeda <dummy@dummy>
2019-03-06 22:24:48 +0900

    2019/3/7の課題

--- JavaTask/src/jp/co/cisys/training/mathematics/ikeda/Main.java
+++ JavaTask/src/jp/co/cisys/training/mathematics/ikeda/Main.java
@@ -4,28 +4,11 @@ public class Main {
 
 	public static void main(String[] args) {
 
-		int[] numList = {912, 12, 44, 122};
-		int gdc = numList[0];
-		int lcm = numList[0];
-		for (int i = 1; i < numList.length; i++) {
-			// 最大公約数
-			int[] ret = moreBThenA(gdc, numList[i]);
-			gdc = new Gdc(ret[0], ret[1]).getGcd();
+		int[] numList = {108, 36, 54, 90};
+		Gdc gdc = new Gdc(numList);
 
-			// 最小公倍数
-			ret = moreBThenA(lcm, numList[i]);
-			lcm = new Gdc(ret[0], ret[1]).getLcm();
-		}
-
-		System.out.println(gdc);
-		System.out.println(lcm);
-	}
-
-	public static int[] moreBThenA(int a, int b) {
-		int[] ret = new int[2];
-		ret[0] = Math.max(a, b);
-		ret[1] = Math.min(a, b);
-		return ret;
+		System.out.println(gdc.getGcd());
+		System.out.println(gdc.getLcm());
 	}
 
 }
