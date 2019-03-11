79e8af44c4f94b9d6cc08ec64a874d3ef361debe
kobayashi-makoto <makoto.kobayashi@cisys.co.jp>
2019-03-11 19:45:41 +0900

    3/11　途中

---
+++ SqlTask/shopping/kobayashi/shopping.sql
@@ -0,0 +1,5 @@
+insert into m_item_category(category_id,category_name,deleted) VALUES (1,'–ìØ',false);
+insert into m_item_category(category_id,category_name,deleted) VALUES (2,'“÷',false);
+insert into m_item_category(category_id,category_name,deleted) VALUES (3,'‰Ùq',true);
+insert into m_item_category(category_id,category_name,deleted) VALUES (4,'ˆù—¿',false);
+select*from m_item_category
