8ff5eead5b6c645f9e14292b4b6dc57ba8908100
kou.ukai <kou.ukai@cisys.co.jp>
2019-03-18 10:21:20 +0900

    WebTask初期コミット

---
+++ WebTask/WEB-INF/src/jp/co/cis/bbs/servlet/BbsServlet.java
@@ -0,0 +1,29 @@
+package jp.co.cis.bbs.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Bbs", urlPatterns = { "/" })
+public class BbsServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		// リクエストパラメータの文字コードを指定
+		req.setCharacterEncoding("UTF-8");
+
+		// TODO リクエストパラメータの取得
+
+		req.getRequestDispatcher("/WEB-INF/jsp/index.jsp").forward(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		super.doPost(req, resp);
+	}
+}
